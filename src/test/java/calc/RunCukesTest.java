e2f2a052849552c003bffbb7e233bf9fa3cde251
Joel Byrnes <joel@adeptusproductions.com>
2014-07-31 16:50:57 +1000

    JB use updated Cucumber API for options

--- src/test/java/calc/RunCukesTest.java
+++ src/test/java/calc/RunCukesTest.java
@@ -1,10 +1,11 @@
 package calc;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(features = "src/test/resources/calc",
+@CucumberOptions(features = "src/test/resources/calc",
 		format = {"html:target/test-reports/cucumber",
 				"junit:target/test-reports/TEST-cukes.xml",
 				"json:target/TEST-cukes.json"})
